2453a75f0b8d6e001d927888f6e70fa7672a1c23
Kojoel <nanaabruquah1s@gmail.com>
2024-06-18 22:19:36 +0000

    Used the call(), apply() and bind() methods on the Object

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-person = {
+//Person object
+const person = {
     name: 'Nana',
     age: '25',
     greet: function(){
         console.log(`Hello my name is ${this.name}, and I'm ${this.age} years old.`)
     }
 }
-
 person.greet();
+
+//Object with new values
+let somePerson = {
+    name: 'Johnny',
+    age: 35,
+}
+
+//call()
+person.greet.call(somePerson);
+
+//apply()
+//Takes in arguments as arrays
+let args = [this.name, this.age]
+person.greet.apply(somePerson, args);
+
+//bind()
+let boundGreet = person.greet.bind(somePerson);
+boundGreet();
